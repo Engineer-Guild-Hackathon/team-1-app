e726bb7466413ce6ef108da0e22dacb8115efa32
yo012412 <monnte9@gmail.com>
2025-09-17 00:34:19 +0900

    commit to be merged

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,17 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient} from '@prisma/client'
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
-
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Discord',
-          content: 'https://pris.ly/discord',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-  {
-    name: 'Bob',
-    email: 'bob@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  }
-]
-
-export async function main() {
-  for (const u of userData) {
-    await prisma.user.create({ data: u })
-  }
+async function main() {
+    await prisma.c
 }
 
 main()
+    .then(async () => {
+        await prisma.$disconnect();
+        process.exit(0);
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
